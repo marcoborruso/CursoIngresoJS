d90d789f5a4c9351c0121be6af8206c997a24188
Sebastian Ovejero <52410377+Sunchoo2k@users.noreply.github.com>
2020-02-29 12:37:25 -0300

    Ultimo repaso

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+ var nombre;
+ var cantidadDePersonas;
+ var cantidadDeDias;
+ var formaDePago;
+ var respuesta;
+ var maximoPersonas;
+ var maximoDeDias;
+ var contadorReservas;
+ var acumuladorReservas;
+ var Promedio;
+ var contadorEfectivo = 0;
+ var contadorQR = 0;
+ var contadorTarjeta = 0;
+ var formaPagoMasUsada;
+
+do{
+
+
+
+} while (respuesta = confirm("Desea continuar?"));
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,75 @@
 function mostrar()
 {
+    var nombre;
+    var edad;
+    var sexo;
+    var nota;
+    var contVaronesAprobados = 0;
+    var contDeMenores = 0;
+    var acumNotaMenores = 0;
+    var contAdolescentes = 0;
+    var acumNotaAdolescentes = 0;
+    var contMayores = 0;
+    var acumMayores = 0;
+    var contFemenino = 0;
+    var contMasculino = 0;
+    var notasMasc = 0;
+    var notasFem = 0;
+           
+    do{
+       nombre = prompt("Ingrese el nombre:");
+       while (!isNaN(nombre)){
+           nombre = prompt("Eso no es un nombre, ingrese un nombre:");
+       }
+       
+       edad = parseInt(prompt("Ingrese la edad:"));
+       while (isNaN(edad) || edad > 100 || edad < 3){
+           edad = parseInt(prompt("Eso no es una edad válida, ingrese la edad:"));
+       }
+       
+       sexo = prompt('Ingrese sexo "F" o "M":').toUpperCase();
+       while(!(sexo == "M" || sexo == "F")){
+           sexo = prompt("Error, ingrese su sexo nuevamente:").toUpperCase();
+       }
+       
+       nota = parseInt(prompt("Ingrese la nota entre 1 y 10:"))
+       while (isNaN(nota) || nota > 10 || nota < 1){
+           nota = parseInt(prompt("Eso no es una nota válida, ingrese la nota (1-10):"));
+        }
+        
+        if (sexo == "M" && nota > 5){
+            contVaronesAprobados++;
+        }
+        if (edad < 13){
+            acumNotaMenores = acumNotaMenores + nota;
+            contDeMenores++;
+        }
+        else if (edad > 18){
+            acumMayores = acumMayores + nota;
+            contMayores++;
+        }
+        else {
+            acumNotaAdolescentes = acumNotaAdolescentes + nota;
+            contAdolescentes++;
+        }
 
+        if (sexo == "M"){
+            notasMasc = notasMasc + nota;
+            contMasculino++;
+        }
+        else {
+            notasFem = notasFem + nota;
+            contFemenino++;
+        }
+         
+       // respuesta = prompt("¿Quiere continuar?").toLowerCase();
+
+    } while (respuesta = confirm("Desea continuar?"));
+    
+    document.write("a) Cantidad de varones aprobados: " + contVaronesAprobados + "</br>");
+    document.write("b) Promedio de notas de menores: " + acumNotaMenores/contDeMenores + "</br>");
+    document.write("c) Promedio de notas de adolescentes: " + acumNotaAdolescentes/contAdolescentes +"</br>");
+    document.write("d) Promedio de notas de mayores: " + acumMayores/contMayores + "</br>");
+    document.write("e) Promedio de notas masculino: " + notasMasc/contMasculino + "</br>");
+    document.write("f) Promedio de notas femenino: " + notasFem/contFemenino + "</br>");
 }
